e0a18aa4dad38d193ffe47afb1877fa7c1c41572
smisi4 <smickey4488@gmail.com>
2024-05-21 10:38:14 +0200

    set stopper routing, add to menu

--- demo/src/app/app.routes.ts
+++ demo/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SzamTippComponent } from './szam-tipp/szam-tipp.component';
+import { StopperComponent } from './stopper/stopper.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent},
     { path: 'szamTipp', component: SzamTippComponent},
+    { path: 'stopper', component: StopperComponent},
     { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
