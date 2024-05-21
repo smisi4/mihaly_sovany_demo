dd6c6b2b248cca4d99577704fbf45dd10d110eb2
smisi4 <smickey4488@gmail.com>
2024-05-21 17:55:02 +0200

    add and set  koPapirOllo component

--- demo/src/app/app.routes.ts
+++ demo/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SzamTippComponent } from './szam-tipp/szam-tipp.component';
 import { StopperComponent } from './stopper/stopper.component';
+import { KoPapirOlloComponent } from './ko-papir-ollo/ko-papir-ollo.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent},
-    { path: 'szamTipp', component: SzamTippComponent},
-    { path: 'stopper', component: StopperComponent},
+    { path: 'home', component: HomeComponent },
+    { path: 'szamTipp', component: SzamTippComponent },
+    { path: 'stopper', component: StopperComponent },
+    { path: 'koPapirOllo', component: KoPapirOlloComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
---
+++ demo/src/app/ko-papir-ollo/ko-papir-ollo.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-ko-papir-ollo',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './ko-papir-ollo.component.html',
+  styleUrl: './ko-papir-ollo.component.css',
+  encapsulation: ViewEncapsulation.None
+})
+
+export class KoPapirOlloComponent {
+
+}
